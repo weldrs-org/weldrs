120bf0e264ff282549f37a81c772519663add690
Paul Stey <paulstey@gmail.com>
2026-02-24 13:28:31 -0500

    refactor: run cargo fmt

--- benches/pipeline.rs
+++ benches/pipeline.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, criterion_group, criterion_main};
 use polars::prelude::*;
 use rand::prelude::*;
 use rand::rngs::StdRng;
